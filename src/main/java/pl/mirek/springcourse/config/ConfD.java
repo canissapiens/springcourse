9b7ac00a6b0b17357c51705249509f05c9d7303a
Miroslaw Hardek <miroslaw.hardek@gmail.com>
2019-09-20 09:10:08 +0200

    20 IX 2019 Lekcja konfiguracja 2

---
+++ src/main/java/pl/mirek/springcourse/config/ConfD.java
@@ -0,0 +1,25 @@
+package pl.mirek.springcourse.config;
+
+import javafx.application.Application;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+// wlasny plik konfiguracyjny
+// uzywamy "/" nie "\"
+// jesli jest "\" to nie dziala w Linuxie
+@PropertySource(value = "file:./conf.ini")
+public class ConfD {
+
+    @Value("${my-param.author}")
+    private String author;
+
+    @EventListener(ApplicationReadyEvent.class)
+    private void greetings() {
+        System.out.println("ConfC.class says: Hello " + author + "!");
+    }
+}
